f860108f31be203cc0e1f4637ce3a77c079c69c8
julianjauk <jaujuc18@htl-kaindorf.at>
2022-11-08 11:09:45 +0100

    feat: requirements.txt

--- video_face_detection.py
+++ video_face_detection.py
@@ -1,11 +1,12 @@
-import cv2
-import numpy as np
 import os
 import time
+
+import cv2
 import youtube_dl
 
 
 def process_video(video_url, name):
+    counter = 0
     ydl_opts = {}
     ydl = youtube_dl.YoutubeDL(ydl_opts)
     info_dict = ydl.extract_info(video_url, download=False)
@@ -23,39 +24,45 @@ def process_video(video_url, name):
                 print('video not opened')
                 exit(-1)
 
-            while True:
+            while True and (counter <= 1200):
                 ret, frame = cap.read()
                 if not ret:
                     break
 
-                seek_faces(frame, name)
-            print('DONE!')
+                if seek_faces(frame, name):
+                    counter += 1
+
             cap.release()
 
     cv2.destroyAllWindows()
 
 
 def seek_faces(source, name):
+    found = False
+    ## targets the haarcascade file
     haar_file = './haar_cascade/haarcascade_frontalface_default.xml'
     face_cascade = cv2.CascadeClassifier(haar_file)
-
     frame_detected = face_cascade.detectMultiScale(source, 1.3, 5)
-    face_counter = 0
-    size = (width, height) = (250, 250)
 
-    for (x, y, width, height) in frame_detected:
-        print('detected!')
+    for face in frame_detected:
         face_name = f'./frames/{name}/{name}_{time.time()}.jpg'
         cv2.imwrite(face_name, source)
-        face_counter += 1
-    pass
+        print(face_name)
+        found = True
+
+    return found
 
 
 if __name__ == '__main__':
-    name = "gu"
-    try:
-        if not os.path.exists(f'frames/{name}'):
-            os.system(f'mkdir frames/{name}')
-    except OSError:
-        print(f'Error occured')
-    process_video("https://www.youtube.com/watch?v=J3MSyvJSn9A", name)
+    videos = [
+        ["renate-bauer", "https://www.youtube.com/watch?v=wcG45zXcgtM"],
+        ["walter-white", "https://www.youtube.com/watch?v=H1TjhNHlJGs"],
+    ]
+    for video in videos:
+        try:
+            if not os.path.exists(f'frames/{video[0]}'):
+                os.system(f'mkdir frames/{video[0]}')
+        except OSError:
+            print(f'Error occured')
+
+        process_video(video[1], video[0])
