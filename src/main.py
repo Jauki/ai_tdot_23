6baff16ac1c4e5211a33d374af9c2643dd01f70b
JakobGalaxy <royerpaul11@gmail.com>
2022-11-22 12:16:16 +0100

    fix : copy the region of interest to ensure clean data

--- src/main.py
+++ src/main.py
@@ -3,16 +3,18 @@ import cv2
 import os
 import numpy as np
 from face_recognition.face_recognition_service import FaceRecognitionService, FaceRecognitionResult
-from personaldata_recognition.personaldata_recognition_service import PersonalDataRecognitionService, PersonalDataRecognitionResult
+from personaldata_recognition.personaldata_recognition_service import PersonalDataRecognitionService, \
+    PersonalDataRecognitionResult
 
 CWD: os.path = os.getcwd()
 FACE_CASCADE_CLASSIFIER_PATH: os.path = os.path.join(CWD, 'haar_cascade', 'haarcascade_frontalface_default.xml')
 FACE_RECOGNITION_SERVICE_MODEL_PATH: os.path = os.path.join(CWD, 'face_recognition', 'model-storage',
                                                             'face-recognition-model-last-state')
 AGE_DETECTION_MODEL_PATH: os.path = os.path.join(CWD, 'personaldata_recognition', 'model-storage',
-                                                            'age_model.h5')
+                                                 'age_model.h5')
 GENDER_DETECTION_MODEL_PATH: os.path = os.path.join(CWD, 'personaldata_recognition', 'model-storage',
-                                                            'gender_model.h5')
+                                                    'gender_model.h5')
+
 
 class Main:
     def __init__(self, face_cascade_classifier_path: os.path = FACE_CASCADE_CLASSIFIER_PATH):
@@ -65,10 +67,10 @@ class Main:
             # classify every face (and display)
             for (x, y, w, h) in face_detections:
                 # this is passed to all services
-                region_of_interest: np.ndarray = frame[y:y + h, x:x + w]
+                region_of_interest: np.ndarray = np.copy(frame[y:y + h, x:x + w])
 
                 # highlight ROI
-                color = (255, 2555, 255)  # color in BGR
+                color = (255, 255, 255)  # color in BGR
                 stroke = 2
                 cv2.rectangle(frame, (x, y), (x + w, y + h), color, stroke)
 
@@ -90,9 +92,11 @@ class Main:
         cv2.putText(frame, f'{result.label}', (x, y - 40), font, 1, color, stroke, cv2.LINE_AA)
         cv2.putText(frame, f'{probability_str}%', (x, y - 10), font, 1, color, stroke, cv2.LINE_AA)
 
-    def __execute_personaldata_recognition_service(self, frame: np.ndarray, region_of_interest: np.ndarray, x: int, y: int, height: int):
+    def __execute_personaldata_recognition_service(self, frame: np.ndarray, region_of_interest: np.ndarray, x: int,
+                                                   y: int, height: int):
         # use face-recognition-service
-        result: PersonalDataRecognitionResult = self.__personaldata_recognition_service.predict_frame(region_of_interest)
+        result: PersonalDataRecognitionResult = self.__personaldata_recognition_service.predict_frame(
+            region_of_interest)
 
         # draw face-recognition-service result
         font = cv2.FONT_HERSHEY_SIMPLEX
